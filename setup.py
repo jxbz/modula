4925ff4a21057e4d11d12d35d85c55a89b49bd53
Jeremy Bernstein <jembernstein@gmail.com>
2024-05-23 23:21:18 -0400

    tweaks

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ import setuptools
 setuptools.setup(
     name="modula",
     packages=setuptools.find_packages(),
-    version="0.0.1",
+    version="0.0.0.0.0.1",
     author="Jeremy Bernstein",
     author_email="jbernstein@mit.edu",
     description="Automatically normalize NN training in the modular norm.",
-    url="git@github.com:jxbz/modula.git",
+    url="https://github.com/jxbz/modula",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
